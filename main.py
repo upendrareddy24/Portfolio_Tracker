103fd9044e7251e117269190eb4fe8dc49329c8a
Bhavana5N <34437924+Bhavana5N@users.noreply.github.com>
2025-12-17 23:29:46 -0500

    Robust static file serving for Heroku

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_folder='.', static_url_path='')
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('.', 'index.html')
-
-@app.route('/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('.', filename)
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
